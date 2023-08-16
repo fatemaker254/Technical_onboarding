82dcc698fc1fb7d88a454ec1f1031a7b1be39f89
fatemaker254 <dassoham254@gmail.com>
2023-08-16 20:46:49 +0530

    image correction

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "2ndbackground.jpg";
+import image from "../images/2ndbackground.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "1stbackground.jpg";
+import image from "../images/1stbackground.jpg";
 
 const imageAltText = "Solid image backfround";
 
